bb56dc642e606e3c9db0a11f02d8906d01352504
= <=>
2024-01-22 16:04:03 +0600

    Main Method  added

--- src/Factory.java
+++ src/Factory.java
@@ -1,6 +1,11 @@
 public class Factory {
     private static Factory ins = null ;
     private Factory(){}
+
+    public static Factory getInstance(){
+        if(ins == null) ins = new Factory() ;
+        return  ins ;
+    }
     public shape getShape(String shapeType){
         if(shapeType == null){
             return null;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Factory fc = Factory.getInstance();
+        Factory fc2 = Factory.getInstance();
+        System.out.println("Obj 1 : " + fc.hashCode());
+        System.out.println("Obj 2 : " + fc2.hashCode());
+
+        shape sq = fc.getShape("SQUARE"); sq.draw();
+        shape rq = fc.getShape("RECTANGLE"); rq.draw();
+
 
     }
 }
